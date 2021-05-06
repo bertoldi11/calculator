ea3c2f71a946f9aaa2f95c327f126fbb9c6dd17e
Vinícius Rocha <vinicius.rocha@somoseducacao.com.br>
2021-05-05 21:19:07 -0300

    Calculating product's price based on its type

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,13 @@ class Product < ApplicationRecord
   validates :tax_formula, presence: true
 
   has_one :type
+
+  before_create :calculate_price_by_type
+
+  private
+   def calculate_price_by_type
+     type = Type.find(self.type_id)
+     value_string = "#{self.price} #{type.operation} #{type.value}"
+     self.price = eval(value_string)
+   end
 end
